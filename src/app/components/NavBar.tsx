1c36e101464ece305289da090e41bf692f1e031b
Adria Navarro <adrk3010@gmail.com>
2025-07-07 12:58:41 -0500

    styling the nav bar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,10 +1,10 @@
 export default function NavBar() {
   return (
     <nav
-      className="flex flex-row items-center md:px-4 md:text-xl z-20 bg-teal-700
-     justify-end w-full h-16 sticky top-0 gap-12 shadow-lg"
+      className="flex flex-row items-center md:px-4 text-sm md:text-xl z-20 bg-teal-700
+     justify-end w-full h-16 sticky top-0 gap-5 md:gap-12 shadow-lg"
     >
-      <MenuItem href="#about-me">About Me</MenuItem>
+      <MenuItem href="#about-me" className="ml-8">About Me</MenuItem>
       <MenuItem href="#personal-projects">Personal Projects</MenuItem>
       <MenuItem href="#bottom">Contact</MenuItem>
       <div></div>
@@ -15,10 +15,11 @@ export default function NavBar() {
 type MenuItemProps = {
   children?: React.ReactElement | string;
   href?: string;
+  className?: string;
 }
 
-function MenuItem({children, href}: MenuItemProps) {
+function MenuItem({children, href, className}: MenuItemProps) {
   return (
-    <a className="cursor-pointer" href={href}>{children}</a>
+    <a className={`cursor-pointer ${className}`} href={href}>{children}</a>
   )
 }
