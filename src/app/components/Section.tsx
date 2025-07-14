bda50e356031c2aa982c3833189b38f472a81c75
Adria Navarro <adrk3010@gmail.com>
2025-07-14 11:22:49 -0500

    fix overflow issue

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Section({ title, className, children, id }: Props) {
   return (
-    <section className="px-4 md:px-8 flex flex-col w-full min-h-[80vh]" id={id}>
+    <section className="px-4 md:px-8 flex flex-col w-full min-h-[80vh] overflow-x-hidden" id={id}>
       <div className="relative inline-block self-center mt-16">
       <span className="relative z-10 text-3xl md:text-4xl xl:text-5xl">{title}</span>
       <div className="absolute w-32 md:w-40 h-5 md:h-5 bg-teal-800 -right-5 bottom-0 z-0" />
